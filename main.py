3ceba12748ce5872ce83c7327326fda9ac3d85f0
Loredana-Manea-B1 <76653089+Loredana-Manea-B1@users.noreply.github.com>
2022-01-05 00:14:50 +0200

    am facut progres
    Butoane pagina principala , luat informatii de pe wikipedia

--- main.py
+++ main.py
@@ -1,29 +1,85 @@
+import re
+from time import strftime
+import requests
 
 
-import time
-import tkinter as tk
-
-root = tk.Tk()
-
-
-def close():
-    root.destroy()
+def month_and_day(asString=True):
+    if (asString):
+        return strftime("%B_%d")
+    return strftime("%m_%d")
 
 
-def show():
-    root.deiconify()
+def extractLines(rawtext):
+    rawlist = rawtext.split('\n')
+    eventlist = []
+    eventswitch = False
+    for curline in rawlist:
+        # Only extract Birthdays and Events.
+        if 'id="Events"' in curline:
+            eventswitch = True
+        if 'id="Births"' in curline:
+            eventswitch = False
+        if eventswitch:
+            eventlist.append(curline)
+
+    # This code will help clean up some of the wikipedia formatting
+    startrecord = False
+    parsedlist = []
+    for curline in eventlist:
+        if '<li>' in curline:
+            startrecord = True
+        if startrecord == True:
+            modline = curline.replace('<li>', '')
+            modline = modline.replace('</li>', '')
+            modline = modline.replace('<lu>', '')
+            modline = modline.replace('</lu>', '')
+            modline = modline.replace('<ul>', '')
+            modline = modline.replace('</ul>', '')
+            modline = modline.replace('</a>', '')
+            modline = modline.replace('<a *>', '')
+            modline = modline.replace('<i>', '')
+            modline = modline.replace('</i>', '')
+            modline = modline.replace('<u>', '')
+            modline = modline.replace('</u>', '')
+            modline = modline.replace('&#8211; ', '')
+            subbed = re.sub('<a.*?>', '', modline)
+            parsedlist.append(subbed)
+        if '</li>' in curline:
+            startrecord = False
+
+    return parsedlist
 
 
-def hide():
-    root.withdraw()
-    time.sleep(5)
-    show()
+############################################################
+###### Begin main body of code below #######################
+############################################################
+
+# Command line arguments here.
+
+# parser.add_argument('-w', '--html', action='store_true', help='Output an html with the fact of the day.')
+# parser.add_argument('-p', '--printOnly', action='store_true', help='Will only print the fact.')
+# args = parser.parse_args()
 
 
-tk.Frame(root, width=250, height=100).pack()
-button = tk.Button(text='Continua', command=hide)
-button.pack()
-button1 = tk.Button(text='Stop', command=close)
-button1.pack()
 
-root.mainloop()
+urlToGet = 'https://simple.wikipedia.org/wiki/' + month_and_day(True)
+request = requests.get(urlToGet)
+eventLines = extractLines(request.text)
+fEvents = open('EventsOfToday.txt', 'w')
+
+# Write all facts out to a file.
+for line in eventLines:
+    fEvents.write(line + '\n')
+
+randomLine = eventLines[randomChoice]
+print("A random event from this day in history...")
+print("On " + strftime("%B %d, ") + randomLine)
+if not args.printOnly:
+    if doHTML:
+        fhtml = open('todaysevent.html', 'w')
+        fhtml.write("A random event in history...\n")
+        fhtml.write(' '.join(["On", strftime("%B %d,"), line]))
+        fhtml.close()
+        webbrowser.open("todaysevent.html")
+
+print("Done")
